01424f55771d28075925dde4ca96e0feac348e1f
Dustin <dyelland@gmail.com>
2017-10-09 15:37:21 -0500

    wrote some fun validations

--- frontEnd/src/reducers/list-reducer.js
+++ frontEnd/src/reducers/list-reducer.js
@@ -1,7 +1,19 @@
 const initialState = [];
 
+const validateList = (payload,options) => {
+  console.log('__Options__',options);
+  let { validateId,validateTitle } = options;
+  if(validateId && !payload._id){
+    throw new Error('Validation error: No _id found');
+  }
+  if(validateTitle && !payload.title){
+    throw new Error('Validation error: no title found');
+  }
+};
+
 export default (state = initialState,action = {}) => {
   const { type, payload } = action;
+
 
   switch(type){
     case 'LIST_CREATE':
@@ -10,6 +22,7 @@ export default (state = initialState,action = {}) => {
         payload
       ];
     case 'LIST_REMOVE':
+      validateList(payload,{validateId: true});
       return state.filter(list => {
         return list._id !== payload._id;
       });
